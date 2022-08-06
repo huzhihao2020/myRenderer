deb5269ad7081e41542f7bf1aa1b86884eccf353
huzhihao <1162287492@qq.com>
2022-08-06 10:46:50 +0800

    Update build-macos.sh

--- build-macos.sh
+++ build-macos.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-rm -rf build/CMakeFiles
-rm -f  build/CMakeCache.txt
+rm -rf build/
 cmake -S . -B build -G Xcode -DCMAKE_EXPORT_COMPILE_COMMANDS=1
-# cmake . --build ./build --target GEngine --config Release
